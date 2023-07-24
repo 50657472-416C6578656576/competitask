3ae3ef7c6e06d43e325c9da4005e5bdd0c3c11f7
Nick <s21d_kovtun@179.ru>
2023-07-25 02:12:58 +0300

    auth fixes

--- BACK/app/auth/security_utils.py
+++ BACK/app/auth/security_utils.py
@@ -1,8 +1,9 @@
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
-import os
 import time
-from typing import Union, Any
 from jose import jwt
+
 
 ACCESS_TOKEN_EXPIRE_SECONDS = 1800  # 30 minutes
 REFRESH_TOKEN_EXPIRE_SECONDS = 60 * 60 * 24 * 7  # 7 days
@@ -32,3 +33,19 @@ def create_access_token(subject: str, expires_delta: int = None) -> str:
 def decode_token(token: str) -> str | None:
     decoded = jwt.decode(token, JWT_SECRET_KEY, algorithms=ALGORITHM)
     return decoded['user_id'] if time.time() < decoded['exp'] else None
+
+
+class JwtBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JwtBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JwtBearer, self).__call__(request)
+        if credentials and credentials.scheme == "Bearer":
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid Token!")
+
+    @staticmethod
+    def verify_jwt(jwt_token: str):
+        return bool(decode_token(jwt_token))
